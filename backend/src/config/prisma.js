f726aef77488e8c2c717f6499a1d0c646ff1a148
nimishog <nimishog@gmail.com>
2026-08-20 11:51:05 +0000

    build(backend/config/prisma.js) updated prisma config file according to prisma 7.0 standards

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,11 @@
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-// Single shared Prisma instance for the whole app.
-const prisma = new PrismaClient();
+const { Pool } = pg;
 
-export { prisma };
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
